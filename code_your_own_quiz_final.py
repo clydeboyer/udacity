6ee949d2679e7af2d920fb558a7f305b8a3ee395
Clyde Boyer <clydeboyer@users.noreply.github.com>
2017-07-09 21:33:19 -0400

    Update code_your_own_quiz_final.py

--- code_your_own_quiz_final.py
+++ code_your_own_quiz_final.py
@@ -8,34 +8,34 @@
 
 # ------ Define the EASY Quiz -----
 quiz_easy = '''
-/////////////////////////////////////////////////
+__________________________________________________
 
 You chose the EASY Quiz. You will get 5 guesses per problem. Let's get started. HINT: Remember to CAPITALIZE capitals :)
 
-Americans are notoriously bad at geography. To help us improve our basic understanding of Europe, memorize and name the capitals of the following European cities. ___1___ is the capital of the Emerald Isle, Ireland. Kelly Pickler infamously missed this next question on a U.S game show: The capital of France is ___2___ , otherwise known as la Ville des Lumieres. ___3___ is the capital of the Netherlands. ___4___ is the capital of Spain. The following country and capital have the same name. To help you out a bit, this country is known for Grand Prix, casinos and Princess Grace Kelly. ___5___ is the capital of ___5___.
+Americans are notoriously bad at geography. To help us improve our basic understanding of Europe, memorize and name the following European capitals. ___1___ is the capital of the Emerald Isle, Ireland. Kelly Pickler infamously missed this next question on a U.S game show: The capital of France is ___2___ , otherwise known as la Ville des Lumieres. ___3___ is the capital of the Netherlands. ___4___ is the capital of Spain. The following country and capital have the same name. To help you out a bit, this country is known for Grand Prix, casinos and Princess Grace Kelly. ___5___ is the capital of ___5___.
 
-/////////////////////////////////////////////////
+__________________________________________________
 '''
 # ------ Define the MEDIUM Quiz -----
 quiz_medium = '''
-/////////////////////////////////////////////////
+__________________________________________________
 
-You chose the MEDIUM Quiz. Time to break a little sweat. You will get 5 guesses per problem. Let's get started.
+You chose the MEDIUM Quiz. Time to break a little sweat. You will get 4 guesses per problem. Let's get started.
 
 Name the seven taxonomic ranks for life forms in order: ___1___, ___2___, ___3___, ___4___, ___5___, ___6___, ___7___.
 
-/////////////////////////////////////////////////
+__________________________________________________
 '''
 
 # ------ Define the ZOIKS Quiz -----
 quiz_zoiks = '''
-/////////////////////////////////////////////////
+__________________________________________________
 
-You chose the ZOIKS Quiz. You will get 5 guesses per problem. Let's get started.
+You chose the ZOIKS Quiz. You will get 3 guesses per problem. Let's get started.
 
 Rocks are for more than just throwing at your friends. Rocks classified as ___1___ undergo a physical change due to extreme heat and pressure (hint: think Metamorphin' Power Rocks). Rocks classified as ___2___, form when particles from other rocks or the remains of plants and animals are pressed and cemented together (Hint: kind of like your sedentary uncle). Rocks classified as ___3___ are formed from the cooling of molten rock at or below the surface. A ___4___ is the lucky scientist who gets to study rocks in the hot sun.
 
-/////////////////////////////////////////////////
+__________________________________________________
 '''
 
 # Define ANSWERS for each quiz
@@ -76,7 +76,7 @@ def start():
 # provide number of remaining attempts
 # prompt user to answer questions
 # if user input = correct answer, replace BLANKS in QUIZ with user input
-# if no more attempts end game
+# if no more attempts, end game
 # if user input != correct answer, increment -1 on attempts
 
 def quiz_game(quiz, answers, attempts):
@@ -84,21 +84,23 @@ def quiz_game(quiz, answers, attempts):
     index = 0
     while index < len(answers):
         print "\n" + "> You have [ " + str(attempts) + " ] remaining attempts! \n"
-        user_input = raw_input("Please provide the correct response for " + str(blanks[index]) + "\n")
+        user_input = raw_input("Please provide the correct response for " + str(blanks[index]) + " above" + "\n")
         if user_input == answers[index]:
             quiz = quiz.replace(blanks[index], answers[index])
             index = index + 1
             if index == len(answers):
-                return "\nWell Done! You are a Quiz Master! \n"
-            else:
-                print "\nNice job. You answered correctly. Keep going and answer the next question."
                 print quiz
+                return "\n ========================================== \n *** Well Done! You are a Quiz Master! *** \n ========================================== \n"
+            else:
+                print quiz
+                print "\n ------------------------------------------------------ \n Nice job. You answered correctly. Keep going and answer the next question. \n ------------------------------------------------------ \n"
         else:
             if attempts == 1:
-                return "\nGame Over! Keep studying and better luck next time."
+                return "\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n GAME OVER! Sorry, but you have no more attempts. \n Keep studying and better luck next time. \n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   \n"
             else:
                 attempts = int(attempts) - 1
-                print "\nDoh! Missed! But, don't give up hope. You still have " + str(attempts) + " remaining attempts. \n"
+                print "\n ------------------------------------------------------ \n Doh! Incorrect Response! But, don't give up hope. You still have " + str(attempts) + " remaining attempts. \n ------------------------------------------------------ \n"
 
 
 print start()
+
